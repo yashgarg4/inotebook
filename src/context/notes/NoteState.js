b9ef96f508d4241699bdce03284a4d95acff1b3b
yashgarg4 <yashgarg04072001@gmail.com>
2021-10-18 19:56:01 +0530

    a

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,83 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name": "yash",
-        "class": "12"
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name": "laksh",
-                "class": "10c"
-            })
-        }, 1000);
-    }
+   const notesInitial = [
+    {
+      "_id": "6167fa95641587e754d24d1c",
+      "user": "6166c0df878f6defa45e9f64",
+      "title": "new note",
+      "description": "this is a new note",
+      "tag": "business",
+      "date": "2021-10-14T09:38:30.187Z",
+      "__v": 0
+    },
+    {
+      "_id": "616d72c859bc829232c169d3",
+      "user": "6166c0df878f6defa45e9f64",
+      "title": "context note",
+      "description": "contex note is cool",
+      "tag": "businesssss",
+      "date": "2021-10-18T13:12:40.897Z",
+      "__v": 0
+    },
+    {
+        "_id": "6167fa95641587e754d24d1c",
+        "user": "6166c0df878f6defa45e9f64",
+        "title": "new note",
+        "description": "this is a new note",
+        "tag": "business",
+        "date": "2021-10-14T09:38:30.187Z",
+        "__v": 0
+      },
+      {
+        "_id": "616d72c859bc829232c169d3",
+        "user": "6166c0df878f6defa45e9f64",
+        "title": "context note",
+        "description": "contex note is cool",
+        "tag": "businesssss",
+        "date": "2021-10-18T13:12:40.897Z",
+        "__v": 0
+      },{
+        "_id": "6167fa95641587e754d24d1c",
+        "user": "6166c0df878f6defa45e9f64",
+        "title": "new note",
+        "description": "this is a new note",
+        "tag": "business",
+        "date": "2021-10-14T09:38:30.187Z",
+        "__v": 0
+      },
+      {
+        "_id": "616d72c859bc829232c169d3",
+        "user": "6166c0df878f6defa45e9f64",
+        "title": "context note",
+        "description": "contex note is cool",
+        "tag": "businesssss",
+        "date": "2021-10-18T13:12:40.897Z",
+        "__v": 0
+      },
+      {
+        "_id": "6167fa95641587e754d24d1c",
+        "user": "6166c0df878f6defa45e9f64",
+        "title": "new note",
+        "description": "this is a new note",
+        "tag": "business",
+        "date": "2021-10-14T09:38:30.187Z",
+        "__v": 0
+      },
+      {
+        "_id": "616d72c859bc829232c169d3",
+        "user": "6166c0df878f6defa45e9f64",
+        "title": "context note",
+        "description": "contex note is cool",
+        "tag": "businesssss",
+        "date": "2021-10-18T13:12:40.897Z",
+        "__v": 0
+      }
+  ]
+  const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
